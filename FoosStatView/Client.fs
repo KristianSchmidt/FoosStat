c61e00ed4d8515c7e5e34a24ddebc260d6ed0f39
Kristian Bjerre Schmidt <kristian@kreutz.us>
2015-05-14 21:34:54 +0200

    Initial commit
    Initial commit

---
+++ FoosStatView/Client.fs
@@ -0,0 +1,99 @@
+namespace FoosStatView
+
+open IntelliFactory.WebSharper
+open IntelliFactory.WebSharper.Formlets
+open IntelliFactory.WebSharper.JavaScript
+open IntelliFactory.WebSharper.Html.Client
+open Domain
+open Parser
+open Analytics
+
+[<JavaScript>]
+module Client =
+
+    [<Literal>]
+    let game = @"set 1
+b5,b5,b3,g_b
+r5,r3,r2,b2,g_b
+r5,r3,g_r
+b5,b2,b5,r5,r3,b2,b3,b2,r2,r2,b2,r2,r3,g_r
+b5,b2,b3,r2,b3,g_b
+r5,r3,b2,b2,g_b
+r5,r3,g_r
+b5,r5,b5,g_b
+set 2
+r5,r3,b2,r5,r3,b2,b2,r2,g_r
+b5,b3,r2,b2,r2,r3,r3,g_r
+b5,b5,b3,g_b
+r5,b5,r2,r3,g_r
+b5,b3,g_b
+r5,r3,b2,r5,b5,r5,r3,b2,r2,b2,b2,r2,b2,b3,g_b
+r5,r5,r3,g_r
+b5,b3,g_b
+r5,r3,r3,r5,r3,r2,r2,r2,b2,b5,b3,r2,r2,r2,r2,b2,g_b
+set 3
+r5,r3,g_r
+b5,b5,r5,r3,g_r
+b5,r5,b2,r3,r5,r3,r3,g_r
+b5,b5,b3,r2,r3,b5,b5,b5,b3,r2,r5,r3,b2,r2,b5,r5,r3,b2,r3,b2,r3,b2,b5,b3,g_b
+r5,r3,r2,b2,b5,b5,b3,g_b
+r5,r3,g_r
+b5,b5,b3,r5,b5,r5,r3,b5,b2,b3,r2,r3,g_r
+set 4
+b5,r2,r3,g_r
+b5,b5,r2,b2,b2,r3,g_r
+b5,r5,r3,g_r
+b5,r3,g_r
+b5,r5,r5,r3,r3,r3,b2,r5,r3,g_r
+set 5
+b5,b5,b3,r2,b2,g_b
+r5,r5,b5,r3,r3,g_r
+b5,r2,r3,g_r
+b5,r5,b2,r2,b2,r2,r3,r3,g_r
+b5,b3,r2,r5,b5,r2,r2,b5,b3,b5,b5,r5,b2,g_b
+r5,b5,b2,r5,r3,b2,r5,b5,r5,r3,g_r
+b5,b5,g_b
+r5,b5,b5,r5,r3,b5,b5,b5,b3,g_b
+r5,r3,r3,g_r
+b5,r2,b5,r2,r5,r3,r3,r3,g_r"
+    
+    let Main () =
+        (*
+        let input = Input [Text ""]
+        let label = Div [Text ""]
+        let test = Attack
+        
+        let selform = ["Attack", Attack; "Defence", Defence]
+                      |> Controls.Select 0
+        
+        Div [
+            label
+            selform
+        ]*)
+
+        let parseGame text =
+            Parser.parseGame text |> Seq.map Parser.parseSet |> List.ofSeq |> Match
+
+        let input = 
+            Controls.ReadOnlyTextArea game
+            |> Enhance.WithSubmitButton
+        let label = Text "Rod"
+        Div [
+            Attr.Class "main-element"
+            label
+            input.Run (fun text -> JS.Alert(sprintf "%A" (parseGame text)))
+        ]
+
+        let summaries = matchSummary foosmatch
+
+        let printMatchSummary (MatchSummary(name,
+                                (col1,PlayerSummary(matchTotal1,setTotals1)),
+                                (col2,PlayerSummary(matchTotal2,setTotals2))
+                                )) =
+            printfn "%s" name
+            printfn "%A\t-\t%A" col1 col2
+            (setTotals1,setTotals2) ||> List.zip |> List.iter (fun (s1,s2) -> printfn "%O\t-\t%O" s1 s2)
+            printfn "Match total: %O\t-\t%O" matchTotal1 matchTotal2
+            printfn ""
+
+        summaries |> List.iter printMatchSummary
---
+++ FoosStatView/Domain.fs
@@ -0,0 +1,68 @@
+﻿namespace Domain
+
+open IntelliFactory.WebSharper
+open IntelliFactory.WebSharper.JavaScript
+
+[<JavaScript>]
+[<AutoOpen>]
+module Domain =
+
+    type Team =
+        | SingleTeam of string
+        | DoubleTeam of string * string
+
+    type PlayerColor = 
+        | Red
+        | Blue
+    
+        member color.otherColor =
+            match color with | Red -> Blue | Blue -> Red
+
+    type MatchTeam =
+        | MatchTeam of Team * PlayerColor
+
+    type Rod =
+        | Defence
+        | Midfield
+        | Attack
+
+    type Event =
+        | Possession of PlayerColor * Rod
+        | Goal of PlayerColor
+
+    let (|SameColor|_|) color =
+        function
+        | Possession(c,_),Possession(c',_) when c = color && c' = color -> Some(SameColor)
+        | Possession(c,_),Goal(c') when c = color && c' = color -> Some(SameColor)
+        | Goal(c),Possession(c',_) when c = color && c' = color -> Some(SameColor)
+        | Goal(c),Goal(c') when c = color && c' = color -> Some(SameColor)
+        | _ -> None
+
+    type Ball = 
+        | Ball of Event list
+
+        with member this.pairwise = match this with
+                                    | Ball(events) -> events |> Seq.pairwise
+
+    type Set =
+        | Set of Ball list
+
+    type Match =
+        | Match of Set list
+
+    type Stat =
+        | NumberStat of num : int
+        | TryFailStat of successes : int * attempts : int
+
+        override this.ToString() =
+            match this with
+            | NumberStat(num) -> sprintf "%i" num
+            | TryFailStat(suc,att) -> sprintf "%i / %i (%.2f)" suc att (100.0 * (float)suc / (float)att)
+
+        static member (+) (s1, s2) =
+            match (s1,s2) with
+            | NumberStat(n1),NumberStat(n2) -> NumberStat(n1+n2)
+            | TryFailStat(suc1,att1),TryFailStat(suc2,att2) -> TryFailStat(suc1+suc2,att1+att2)
+            | _ -> failwith "Type mismatch: Cannot sum different types of stats"
+
+        static member sum (stats : Stat seq) = Seq.reduce (+) stats
