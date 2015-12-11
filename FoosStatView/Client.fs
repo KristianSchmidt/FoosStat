b671abfc67a7894b9969c1156cc8948eafa87c7e
Kristian Bjerre Schmidt <kristian@kreutz.us>
2015-12-11 19:54:39 +0100

    Clean up

--- FoosStatView/Client.fs
+++ FoosStatView/Client.fs
@@ -12,21 +12,21 @@ open Games
 [<JavaScript>]
 module Client = 
     let matchSummaryToTable (ms : MatchSummary) = 
-            let header = Tags.THead [ TR [ TH [ Attr.ColSpan "3"; Attr.Class "text-center"; Text ms.StatName ] ]
-                                      TR [ TH [ Text "" ]; TH [ Text "Red" ]; TH [ Text "Blue" ] ] ]
+        let header = Tags.THead [ TR [ TH [ Attr.ColSpan "3"; Attr.Class "text-center"; Text ms.StatName ] ]
+                                  TR [ TH [ Text "" ]; TH [ Text "Red" ]; TH [ Text "Blue" ] ] ]
             
-            let bodyElement name (redStat : Stat) (blueStat : Stat) =
-                TR [ TD [ Text name ]; TD [ Text (sprintf "%O" redStat) ]; TD [ Text (sprintf "%O" blueStat) ] ]
+        let bodyElement name (redStat : Stat) (blueStat : Stat) =
+            TR [ TD [ Text name ]; TD [ Text (sprintf "%O" redStat) ]; TD [ Text (sprintf "%O" blueStat) ] ]
 
-            let setRow i (redStat : Stat) (blueStat : Stat) = bodyElement (sprintf "Set %i" (i + 1)) redStat blueStat
+        let setRow i (redStat : Stat) (blueStat : Stat) = bodyElement (sprintf "Set %i" (i + 1)) redStat blueStat
 
-            let totalRow =
-                bodyElement "Total" ms.Red.MatchTotal ms.Blue.MatchTotal
-                -< [ Attr.Class "active" ]
+        let totalRow =
+            bodyElement "Total" ms.Red.MatchTotal ms.Blue.MatchTotal
+            -< [ Attr.Class "active" ]
 
-            let body = Tags.TBody ((ms.Red.SetTotals,ms.Blue.SetTotals) ||> List.mapi2 setRow) -< [ totalRow ]
+        let body = Tags.TBody ((ms.Red.SetTotals,ms.Blue.SetTotals) ||> List.mapi2 setRow) -< [ totalRow ]
 
-            Table [ Attr.Class "table table-striped table-hover" ] -< [header; body]
+        Table [ Attr.Class "table table-striped table-hover" ] -< [header; body]
 
     let Main () =
         let parseGame text =
