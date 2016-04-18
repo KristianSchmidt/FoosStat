da99b27e457778f237c612da851dcaec3c552483
Kristian Bjerre Schmidt <kristian@kreutz.us>
2016-04-18 23:36:37 +0200

    Added game schmidt/eskildsen vs hald-bjerrum/savery

--- FoosStat2/Games.fs
+++ FoosStat2/Games.fs
@@ -47,3 +47,43 @@ b5,b5,g_b
 r5,b5,b5,r5,r3,b5,b5,b5,b3,g_b
 r5,r3,r3,g_r
 b5,r2,b5,r2,r5,r3,r3,r3,g_r"
+
+    [<Literal>]
+    let game2 = @"RED:Schmidt/Eskildsen
+BLUE:Hald-Bjerrum/Savery
+set 1
+b5,r2,r5,b2,b3,r2,b5,b3,b2,b3,r2,r5,r5,r3,b2,b3,b5,b3,g_b
+r5,b5,b3,g_b
+r5,r3,g_r
+b5,b5,r5,r3,r3,r2,b2,b2,b3,r2,r5,r3,g_r
+b5,b3,r2,b2,r2,r3,r3,b2,r5,r3,g_r
+b5,b3,g_b
+r5,b5,b3,g_b
+r5,r3,g_r
+b5,b5,b3,g_b
+set 2
+r5,r3,r3,r3,g_r
+b5,b5,r5,r3,g_r
+b5,r5,g_r
+b5,r2,b2,b3,b2,r2,r2,g_r
+b5,b3,r2,b3,g_r
+set 3
+b5,b3,b5,r2,b3,r2,b3,r2,r3,r3,g_r
+b5,r5,r3,b2,g_b
+r5,r5,b5,b3,g_b
+r5,r2,g_r
+b5,b3,g_b
+r5,r3,g_r
+b5,b5,b3,g_b
+r5,r5,b2,g_b
+set 4
+r5,r3,g_r
+b5,r5,b5,r5,r3,b2,b5,b3,g_b
+r5,b5,r2,r2,b2,r2,b2,g_b
+r5,r3,g_r
+b5,b3,b3,b2,r5,r3,b2,b2,b5,b3,b5,b3,b2,b5,b3,g_b
+r5,r3,g_r
+b5,r2,r3,g_r
+b5,b3,g_b
+r5,r3,b2,b5,b3,g_b"
+
