2b9b440f66a0b32539baa469367743baec5f76e5
Kristian Bjerre Schmidt <kristian@kreutz.us>
2016-07-27 21:15:06 +0200

    Removed unneeded stuff from statspage

--- mobile/foosstat/src/StatsPage.fs
+++ mobile/foosstat/src/StatsPage.fs
@@ -21,22 +21,3 @@ module StatsPage =
     
     allBalls.addSubscriber updateSummary
     
-    let currScore = FoosStat.currScore
-    let currBall = FoosStat.currBall
-
-    let restartFromGoal goal fiveBar =
-        currBall.add goal
-        let arr = currBall.toArray()
-        allBalls.add arr
-        currBall.clear()
-        currBall.add fiveBar
-
-    let addGoalBlue _ = restartFromGoal (Goal Blue) (Possession (Red, Midfield))
-    let addGoalRed  _ = restartFromGoal (Goal Red)  (Possession (Blue, Midfield))
-
-    type Testing = { testName : string }
-
-    let testers = Observable.createList [| {testName = "Test 1"}; { testName = "Test 2"}|]
-
-    let addTester _ = testers.add {testName = "Test X"}
-    
